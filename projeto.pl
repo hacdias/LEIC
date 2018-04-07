35937b625f50b75a8a4e67baf870551e07ab653d
Henrique Dias <hacdias@gmail.com>
2018-04-07 17:57:34 +0100

    nova funcao

--- projeto.pl
+++ projeto.pl
@@ -1,11 +1,21 @@
-% Henrique Dias - 89455
+%Nome: Henrique Afonso Coelho Dias: 89455
 
 :- [exemplos_puzzles].
 
-encontra([H|T], X, Res) :-
-  (member(X, H) -> Res = H ; encontra(T, X, Res)), !.
+lista_que_contem([H|T], X, Res) :-
+  (member(X, H) -> Res = H ; lista_que_contem(T, X, Res)), !.
 
 propaga([Listas, _, _], Pos, Posicoes) :-
-  encontra(Listas, Pos, Lista),
+  lista_que_contem(Listas, Pos, Lista),
   append(Lista2, [Pos|_], Lista),
   sort([Pos|Lista2], Posicoes), !.
+
+e_sublista([], _).
+e_sublista([X|Tail], Y):-
+  member(X, Y),
+  e_sublista(Tail, Y).
+
+nao_altera_linhas_anteriores(Posicoes, L, Ja_Preenchidas) :-
+  sort(Posicoes, Posicoes_ordenadas),
+  append(Linhas_anteriores, [(L, _)|_], Posicoes_ordenadas),
+  e_sublista(Linhas_anteriores, Ja_Preenchidas), !.
