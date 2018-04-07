8d622ca461ec517e347fe8a9bb8052fc70a6dff1
Henrique Dias <hacdias@gmail.com>
2018-04-07 17:24:38 +0100

    :)

--- projeto.pl
+++ projeto.pl
@@ -2,15 +2,6 @@
 
 :- [exemplos_puzzles].
 
-antes_de([N|_], N, Res) :-
-  Res = [N], !.
-
-antes_de([H|T], N, Res) :-
-  antes_de(T, N, Res2),
-  Res = [H|Res2], !.
-
-% antes_de(N, L, Result) :- append(Result, [N|_], List), !.
-
 encontra([H|T], X, Res) :-
   (member(X, H) -> Res = H ; encontra(T, X, Res)), !.
 
