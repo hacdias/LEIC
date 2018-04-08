180d917a6cf923d4a60c5ceab42cd1dc1468eb79
Henrique Dias <hacdias@gmail.com>
2018-04-08 15:34:43 +0100

    things

--- projeto.pl
+++ projeto.pl
@@ -1,4 +1,4 @@
-%Nome: Henrique Afonso Coelho Dias: 89455
+% Henrique Afonso Coelho Dias: 89455
 
 :- [exemplos_puzzles].
 
@@ -31,6 +31,8 @@ peso_coluna([(_,X)|T], X, Peso) :-
 peso_coluna([_|T], X, Peso) :-
   peso_coluna(T, X, Peso), !.
 
+% peso_colunas(Posicoes, Dimensao, Resposta)
+
 peso_colunas(Posicoes, 1, Res) :- peso_coluna(Posicoes, 1, Res).
 
 peso_colunas(Posicoes, Dim, Res) :-
@@ -45,13 +47,68 @@ menorOuIgual(X,Y) :-
        ;  false
     ).
 
+comparaPesos([], []).
+
 comparaPesos([H|T], [Z|V]) :-
+  % write(H), write(' '), write(Z), nl,
   menorOuIgual(H, Z),
+  % write('DONE'), nl,
   comparaPesos(T, V).
 
+% verifica_parcial(Puz, Ja_Preenchidas, Dim, Poss)
+
 verifica_parcial([_, _, Maximos], Ja_Preenchidas, Dim, Poss) :- 
+  % nl, nl, write(Maximos), nl, write(Ja_Preenchidas), nl, write(Dim), nl, write(Poss), nl, nl, nl,
   append(Ja_Preenchidas, Poss, Posicoes),
   peso_colunas(Posicoes, Dim, Pesos),
+  % write(Pesos), write(Maximos),
   comparaPesos(Pesos, Maximos), !.
 
-% verifica_parcial(Puz, Ja_Preenchidas, Dim, Poss)
+procura_aux([Listas, Max_Linhas, Max_Cols], (L, C), Total, Ja_Preenchidas, Possibilidades_L) :-
+  length(Max_Linhas, Dim),
+  propaga([Listas, Max_Linhas, Max_Cols], (L, C), Posicoes),
+  nao_altera_linhas_anteriores(Posicoes, L, Ja_Preenchidas),
+  verifica_parcial([Listas, Max_Linhas, Max_Cols], Ja_Preenchidas, Dim, Posicoes),
+  sort(Posicoes, Possibilidades_L), !.
+
+procura(_, [], _, _, P) :- P = [].
+
+procura(Puz, [H|T], Total, Ja_Preenchidas, Possibilidades_L) :-
+  (procura_aux(Puz, H, Total, Ja_Preenchidas, P1) -> true ; P1 = [], true),
+  %write(P1), nl,
+  procura(Puz, T, Total, Ja_Preenchidas, P2),
+  %write(P2), nl,
+  Possibilidades_L = [P1|P2], !.
+
+%poses_linhas([]).
+
+%memberX((L, K), Ja_Preenchidas, L) :- member((L, K), Ja_Preenchidas).
+
+
+possibilidades_linha(Puz, [(L, C)|K], Total, Ja_Preenchidas, Possibilidades_L) :-
+  procura(Puz, [(L, C)|K], Total, Ja_Preenchidas, Posses),
+  % write(Posses), nl,
+  delete(Posses, [], Posses2),
+  write(Posses2), nl,
+  findall(X, aaa(Posses2, X, L, Total), P3),
+  %write(L), nl,
+  %findall(X, memberX(X, Ja_Preenchidas, L), Teste), write(Teste), nl,
+  sort(P3, Possibilidades_L),
+  % obter linha ja preenchida e adicionar a todas as possibilidades_L
+  % sort(Ja_Preenchidas, Preenchidas_Sorted),
+    !.
+
+aaa(As, Bs, Line, Len) :-
+  comb(As, Cs),
+  sort(Cs, Ds),
+  findall(X, member((Line, X), Ds), Fs),
+  length(Fs, Len),
+  Bs = Ds.
+
+comb(As,Bs) :-
+  same_length(As,Full),
+  ASA = [_|_],
+  prefix(ASA,Full),
+  foldl(select,ASA,As,_),
+  flatten(ASA, ASS),
+  sort(ASS, Bs).
