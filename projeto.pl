ef6aa1f60b55cbf726e825ac2c18abb49fccc5f6
Henrique Dias <hacdias@gmail.com>
2018-04-09 09:25:23 +0100

    adicionar comentarios

--- projeto.pl
+++ projeto.pl
@@ -2,25 +2,46 @@
 
 :- [exemplos_puzzles].
 
-lista_que_contem([H|T], X, Res) :-
-  (member(X, H) -> Res = H ; lista_que_contem(T, X, Res)), !.
+%---------------------------------------------------------------------
+% lista_que_contem(Listas, El, Lista) : dada a lista de listas Listas
+% e o elemento El, Lista é a lista pertencente a Listas que contem El.
+% --------------------------------------------------------------------
+lista_que_contem([H|T], El, Lista) :-
+  (member(El, H) -> Lista = H ; lista_que_contem(T, El, Lista)).
+
+%---------------------------------------------------------------------
+% propaga(Puz, Pos, Posicoes) : dado o puzzle Puz, o preenchimento da
+% posicao Pos implica o preenchimento de todas as posicoes da lista
+% ordenada Posicoes.
+% --------------------------------------------------------------------
 
 propaga([Listas, _, _], Pos, Posicoes) :-
   lista_que_contem(Listas, Pos, Lista),
   append(Lista2, [Pos|_], Lista),
   sort([Pos|Lista2], Posicoes), !.
 
+%---------------------------------------------------------------------
+% e_sublista(Lista1, Lista2) : é True se a lista Lista1 estiver contida
+% na lista LIsta2.
+% --------------------------------------------------------------------
+
 e_sublista([], _).
 e_sublista([X|Tail], Y):-
   member(X, Y),
-  e_sublista(Tail, Y).
+  e_sublista(Tail, Y), !.
+
+% --------------------------------------------------------------------
+% nao_altera_linhas_anteriores(Posicoes, L, Ja_Preenchidas) : dada uma
+% lista de posicoes Posicoes, representado a possibilidade de preenchimento
+% para a linha L, todas as posicoes desta lista pertencendo a linhas
+% anterior a L, pertencem a lista de posicoes Ja_Preenchidas. True se a
+% possibilidade Posicoes nao altera as linhas Ja_Preenchidas.
+% --------------------------------------------------------------------
 
 nao_altera_linhas_anteriores(Posicoes, L, Ja_Preenchidas) :-
   sort(Posicoes, Posicoes_ordenadas),
   append(Linhas_anteriores, [(L, _)|_], Posicoes_ordenadas),
   e_sublista(Linhas_anteriores, Ja_Preenchidas), !.
-
-% peso_coluna(Posicoes, X, Peso)
 
 peso_coluna([], _, 0).
 
@@ -31,8 +52,6 @@ peso_coluna([(_,X)|T], X, Peso) :-
 peso_coluna([_|T], X, Peso) :-
   peso_coluna(T, X, Peso), !.
 
-% peso_colunas(Posicoes, Dimensao, Resposta)
-
 peso_colunas(Posicoes, 1, Res) :- peso_coluna(Posicoes, 1, Res).
 
 peso_colunas(Posicoes, Dim, Res) :-
@@ -41,30 +60,35 @@ peso_colunas(Posicoes, Dim, Res) :-
   peso_colunas(Posicoes, NextDim, Res2),
   flatten([Res2|Col], Res), !.
 
-menorOuIgual(X,Y) :-
-    ( X =< Y
-       -> true
-       ;  false
-    ).
+% --------------------------------------------------------------------
+% menor_igual(X, Y) : X e menor ou igual a Y.
+%
+% compara_pesos(Pesos, Maximos) : a lista de pesos Pesos nao excede
+% a lista de pesos maximos Maximos.
+% --------------------------------------------------------------------
 
-comparaPesos([], []).
+menor_igual(X,Y) :- (X =< Y -> true ; false).
 
-comparaPesos([H|T], [Z|V]) :-
-  % write(H), write(' '), write(Z), nl,
-  menorOuIgual(H, Z),
-  % write('DONE'), nl,
-  comparaPesos(T, V).
+compara_pesos([], []).
 
-% verifica_parcial(Puz, Ja_Preenchidas, Dim, Poss)
+compara_pesos([H|T], [Z|V]) :-
+  menor_igual(H, Z),
+  compara_pesos(T, V).
+
+% --------------------------------------------------------------------
+% verifica_parcial(Puz, Ja_Preenchidas, Dim, Poss) : dado um puzzle Puz,
+% uma lista de posicoes ja preenchidas Ja_Preenchidas, a dimensao Dim
+% do puzzle e uma lista de posicoes Poss que representa uma potencial
+% possibilidade para preencher uma linha, é True se Poss nao viola os
+% totais das colunas.
+% --------------------------------------------------------------------
 
 verifica_parcial([_, _, Maximos], Ja_Preenchidas, Dim, Poss) :- 
-  % nl, nl, write(Maximos), nl, write(Ja_Preenchidas), nl, write(Dim), nl, write(Poss), nl, nl, nl,
   append(Ja_Preenchidas, Poss, Posicoes),
   peso_colunas(Posicoes, Dim, Pesos),
-  % write(Pesos), write(Maximos),
-  comparaPesos(Pesos, Maximos), !.
+  compara_pesos(Pesos, Maximos), !.
 
-procura_aux([Listas, Max_Linhas, Max_Cols], (L, C), Total, Ja_Preenchidas, Possibilidades_L) :-
+procura_aux([Listas, Max_Linhas, Max_Cols], (L, C), Ja_Preenchidas, Possibilidades_L) :-
   length(Max_Linhas, Dim),
   propaga([Listas, Max_Linhas, Max_Cols], (L, C), Posicoes),
   nao_altera_linhas_anteriores(Posicoes, L, Ja_Preenchidas),
@@ -74,29 +98,9 @@ procura_aux([Listas, Max_Linhas, Max_Cols], (L, C), Total, Ja_Preenchidas, Possi
 procura(_, [], _, _, P) :- P = [].
 
 procura(Puz, [H|T], Total, Ja_Preenchidas, Possibilidades_L) :-
-  (procura_aux(Puz, H, Total, Ja_Preenchidas, P1) -> true ; P1 = [], true),
-  %write(P1), nl,
+  (procura_aux(Puz, H, Ja_Preenchidas, P1) -> true ; P1 = [], true),
   procura(Puz, T, Total, Ja_Preenchidas, P2),
-  %write(P2), nl,
   Possibilidades_L = [P1|P2], !.
-
-%poses_linhas([]).
-
-%memberX((L, K), Ja_Preenchidas, L) :- member((L, K), Ja_Preenchidas).
-
-
-possibilidades_linha(Puz, [(L, C)|K], Total, Ja_Preenchidas, Possibilidades_L) :-
-  procura(Puz, [(L, C)|K], Total, Ja_Preenchidas, Posses),
-  % write(Posses), nl,
-  delete(Posses, [], Posses2),
-  write(Posses2), nl,
-  findall(X, aaa(Posses2, X, L, Total), P3),
-  %write(L), nl,
-  %findall(X, memberX(X, Ja_Preenchidas, L), Teste), write(Teste), nl,
-  sort(P3, Possibilidades_L),
-  % obter linha ja preenchida e adicionar a todas as possibilidades_L
-  % sort(Ja_Preenchidas, Preenchidas_Sorted),
-    !.
 
 aaa(As, Bs, Line, Len) :-
   comb(As, Cs),
@@ -112,3 +116,10 @@ comb(As,Bs) :-
   foldl(select,ASA,As,_),
   flatten(ASA, ASS),
   sort(ASS, Bs).
+
+possibilidades_linha(Puz, [(L, C)|K], Total, Ja_Preenchidas, Possibilidades_L) :-
+  procura(Puz, [(L, C)|K], Total, Ja_Preenchidas, Posses),
+  delete(Posses, [], Posses2),
+  write(Posses2), nl,
+  findall(X, aaa(Posses2, X, L, Total), P3),
+  sort(P3, Possibilidades_L), !.
