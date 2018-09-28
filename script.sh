7625dcbfadbdd3bd92ae2142f8da63f9ba11da6e
Henrique Dias <hacdias@gmail.com>
2018-09-28 14:38:51 +0100

    start 3rd class

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for f in $(ls ./inputs/*.txt)
-do
-    echo filename: $f
-    echo number of lines: $(expr $(wc -l < $f) + 1)
-    echo number of interconections: $(grep -c "p" $f)
-    echo =================================
-done
