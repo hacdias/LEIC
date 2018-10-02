4acea38cfe3efc6d81286b3f7730a7dd9d927c35
Henrique Dias <hacdias@gmail.com>
2018-10-02 22:53:40 +0100

    updts
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
+++ CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
@@ -1,55 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include "../lib/list.h"
+#include "../lib/types.h"
 #include "../lib/commandlinereader.h"
 
 #define BINARY "../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver"
 
 int getMaxChildren (int argc, char **argv) {
   if (argc == 1) {
-    return -1;
+    return 0;
   }
 
   return atoi(argv[1]);
 }
 
+typedef struct pinfo {
+  pid_t pid;
+  int state;
+}* pinfo_t;
+
 int main (int argc, char** argv) {
-  int maxChildren = getMaxChildren(argc, argv);
   char **argVector = malloc(sizeof(char) * 256 * 3);
   char *buffer = malloc(sizeof(char) * 256);
-  int args = 0;
-  int pid, state;
-  int actual_children = 0;
+  int maxChildren = getMaxChildren(argc, argv);
+  int children = 0, state, args;
+  pid_t pid;
+  list_t list = list_alloc(NULL);
 
-  while ((args = readLineArguments(argVector, 3, buffer, 256)) != -1) {
+  while (TRUE) {
+    args = readLineArguments(argVector, 3, buffer, 256);
+
     if (args == 2 && !strcmp(argVector[0], "run")) {
+      if (maxChildren && children == maxChildren) {
+        wait(NULL);
+        children--;
+      }
 
       pid = fork();
-      actual_children ++;
 
-      if (pid == 0) {
+      if (pid == (pid_t)0) {
         execl(BINARY, BINARY, argVector[1], (char*)NULL);
-        /* code */
-        exit(0);
-      } else {
-        // MAIN PROCESS
-        if (actual_children == maxChildren && maxChildren != -1) {
-          pid = wait(&state);
-          actual_children--;
-        }      
+        return 0;
+      } else if (maxChildren) {
+        children++;
       }
-    } else if ((args == 1 && !strcmp(argVector[0], "exit"))) {
-      while (actual_children > 0 && (pid = wait(&state))) {
+    } else if (args == 1 && !strcmp(argVector[0], "exit")) {
+      while ((pid = wait(&state)) != -1) {
         printf("CHILD EXITED (PID=%i; return %s)\n", pid, state ? "NOK" : "OK");
-        actual_children--;
       }
-      printf("END.\n");
-      exit(0);
 
+      printf("END.\n");
+      return 0;
     } else {
       printf("Invalid Command\n");
+      return 1;
     }
   }
 
