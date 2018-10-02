5d81730e9b2e13030baf8788b4c7e6adade11550
Henrique Dias <hacdias@gmail.com>
2018-10-02 23:07:12 +0100

    delay printing to end
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
+++ CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
@@ -24,12 +24,20 @@ typedef struct pinfo {
   int state;
 } pinfo_t;
 
+pinfo_t* makepinfo (pid_t pid, int state) {
+  pinfo_t* p = malloc(sizeof(pinfo_t));
+  p->pid = pid;
+  p->state = state;
+  return p;
+}
+
 int main (int argc, char** argv) {
   char **argVector = malloc(sizeof(char) * 256 * 3);
   char *buffer = malloc(sizeof(char) * 256);
   int maxChildren = getMaxChildren(argc, argv);
   int children = 0, state, args;
   pid_t pid;
+  list_t* list = list_alloc(NULL);
 
   while (TRUE) {
     args = readLineArguments(argVector, 3, buffer, 256);
@@ -37,7 +45,7 @@ int main (int argc, char** argv) {
     if (args == 2 && !strcmp(argVector[0], "run")) {
       if (maxChildren && children == maxChildren) {
         pid = wait(&state);
-        printf("CHILD EXITED (PID=%d; return %s)\n", pid, state ? "NOK" : "OK");
+        list_insert(list, makepinfo(pid, state));
         children--;
       }
 
@@ -51,10 +59,19 @@ int main (int argc, char** argv) {
       }
     } else if (args == 1 && !strcmp(argVector[0], "exit")) {
       while ((pid = wait(&state)) != -1) {
-        printf("CHILD EXITED (PID=%i; return %s)\n", pid, state ? "NOK" : "OK");
+        list_insert(list, makepinfo(pid, state));
+      }
+
+      list_iter_t it;
+      list_iter_reset(&it, list);
+
+      while (list_iter_hasNext(&it, list)) {
+        pinfo_t* pinfo = (pinfo_t*)list_iter_next(&it, list);
+        printf("CHILD EXITED (PID=%i; return %s)\n", pinfo->pid, pinfo->state ? "NOK" : "OK");
       }
 
       printf("END.\n");
+      list_free(list);
       return 0;
     } else {
       printf("Invalid Command\n");
