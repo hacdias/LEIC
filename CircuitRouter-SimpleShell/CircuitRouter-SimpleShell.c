ad05d45f12a7f585036227e32c92229624be6589
Rodrigo Sousa <rod_sousa@hotmail.com>
2018-10-10 13:44:42 +0100

    Added Comments to code (needs approval of Henrique)
    Licence: MIT
    Rodrigo Sousa, rod_sousa@hotmail.com

--- CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
+++ CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
@@ -11,6 +11,7 @@
 
 #define BINARY "../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver"
 
+// Function desgined to return the number of MaxChildren from input
 int getMaxChildren (int argc, char **argv) {
   if (argc == 1) {
     return 0;
@@ -19,11 +20,13 @@ int getMaxChildren (int argc, char **argv) {
   return atoi(argv[1]);
 }
 
+// Struct where information about a process can be stored
 typedef struct pinfo {
   pid_t pid;
   int state;
 } pinfo_t;
 
+// Function used to store infromation about a process in pinfo_t struct
 pinfo_t* makepinfo (pid_t pid, int state) {
   pinfo_t* p = malloc(sizeof(pinfo_t));
   p->pid = pid;
@@ -37,34 +40,45 @@ int main (int argc, char** argv) {
   int maxChildren = getMaxChildren(argc, argv);
   int children = 0, state, args;
   pid_t pid;
+  // Inicialization of a list to store pid numbers and exit state
   list_t* list = list_alloc(NULL);
 
   while (TRUE) {
     args = readLineArguments(argVector, 3, buffer, 256);
-
+    
+    // User wishes to run SeqSolver
     if (args == 2 && !strcmp(argVector[0], "run")) {
+      // Reached limit of processes available
       if (maxChildren && children == maxChildren) {
+        // Wait until one child process finishes
         pid = wait(&state);
         list_insert(list, makepinfo(pid, state));
         children--;
       }
 
+      // Creation of a child process
       pid = fork();
-
+      
+      // Child process
       if (pid == (pid_t)0) {
         execl(BINARY, BINARY, argVector[1], (char*)NULL);
         return 0;
+      // Parent process (Main process)
       } else if (maxChildren) {
         children++;
       }
+    // User wishes to exit program
     } else if (args == 1 && !strcmp(argVector[0], "exit")) {
+      // Wait for processes still running
       while ((pid = wait(&state)) != -1) {
+        // Insert information about the process that finished in list
         list_insert(list, makepinfo(pid, state));
       }
 
       list_iter_t it;
       list_iter_reset(&it, list);
 
+      // Iterate over the list and print information about the processes
       while (list_iter_hasNext(&it, list)) {
         pinfo_t* pinfo = (pinfo_t*)list_iter_next(&it, list);
         printf("CHILD EXITED (PID=%i; return %s)\n", pinfo->pid, pinfo->state ? "NOK" : "OK");
