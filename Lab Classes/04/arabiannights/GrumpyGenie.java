50510ef0d678ed8feba82e58662e123314890a10
Henrique Dias <hacdias@gmail.com>
2018-10-19 07:32:39 +0100

    feat: week 4
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

---
+++ Lab Classes/04/arabiannights/GrumpyGenie.java
@@ -0,0 +1,25 @@
+package arabiannights;
+ 
+/**
+ * Behaves like any other genie, but with some differences (only one wish).
+ */
+public class GrumpyGenie extends Genie {
+ 
+	/**
+	 * @param limit
+	 *            the number of wishes to grant (ignored: always grants only one wish).
+	 */
+	public GrumpyGenie(int limit) {
+		super(1);
+	}
+ 
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return (getGrantedWishes() == 1) ? "Grumpy genie has granted a wish."
+				: "Grumpy genie has a wish to grant.";
+	}
+ 
+}
