0bf85cb0154e0168645a1ceb28c2ccb92ac353fb
Henrique Dias <hacdias@gmail.com>
2018-10-29 09:44:29 +0000

    chore: update test script

--- doTest.sh
+++ doTest.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-#TODO: Verify if arguments are the right type
-#ASK: Should we create inputs.res and old while running this script?
-if [ "$#" -ne 2 ]
+usage () {
+    echo -e "usage: $(basename "$0") numTasks inputFile"
+    echo -e "\narguments:"
+    echo -e "\tnumTasks\ta positive integer"
+    echo -e "\tinputFile\tpath to input file (must exist)"
+}
+
+n="$1"
+input="$2"
+
+if [ "$#" -ne 2 ] || ! [[ $n =~ ^[1-9][0-9]*$ ]] || ! [ -f $input ]
 then
-    echo "Invalid Arguments"
+    usage
     exit 1
 fi
 
-N="$1"
-input="$2"
 output="$input.res"
 speedupFile="$input.speedups.csv"
-echo "#threads,exec_time,speedup" >> $speedupFile
+echo "#threads,exec_time,speedup" > $speedupFile
 
 ./CircuitRouter-SeqSolver/CircuitRouter-SeqSolver "$input"
 seqTime=$( echo $(grep -o 'Elapsed time    = .*' "$output") | cut -f 4 -d " ")
 echo "1S,$seqTime,1" >> $speedupFile
 
-for i in $(seq 1 $N)
+for i in $(seq 1 $n)
 do
     ./CircuitRouter-ParSolver/CircuitRouter-ParSolver "$input" -t $i
     parTime=$( echo $(grep -o 'Elapsed time    = .*' "$output") | cut -f 4 -d " ")
