59401b3d30cccda1e97c057eb12b98954d257405
Henrique Dias <hacdias@gmail.com>
2018-12-01 19:47:27 +0000

    fix: answer survey

--- sth-core/src/sth/Student.java
+++ sth-core/src/sth/Student.java
@@ -1,19 +1,22 @@
 package sth;
 
 import sth.exceptions.MaximumDisciplinesExceededException;
+import sth.exceptions.NoSuchDisciplineNameException;
 import java.io.Serializable;
-import java.util.TreeSet;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Set;
 
 public class Student extends Person {
   private static final long serialVersionUID = 201810051538L;
 
   private final int MAX_DISCIPLINES = 6;
   private Course _course;
-  private TreeSet<Discipline> _disciplines;
+  private TreeMap<String, Discipline> _disciplines;
 
   Student(int id, String phoneNumber, String name) {
     super(id, phoneNumber, name);
-    _disciplines = new TreeSet<Discipline>();
+    _disciplines = new TreeMap<String, Discipline>(new StringComparator());
   }
 
   public void setCourse(Course c) {
@@ -25,14 +28,26 @@ public class Student extends Person {
   }
 
   public void addDiscipline(Discipline d) throws MaximumDisciplinesExceededException {
+    if (_disciplines.get(d.getName()) != null)
+      return;
+
     if (_disciplines.size() >= MAX_DISCIPLINES)
       throw new MaximumDisciplinesExceededException(getId());
 
-    _disciplines.add(d);
+    _disciplines.put(d.getName(), d);
+  }
+
+  public Discipline getDiscipline(String name) throws NoSuchDisciplineNameException {
+    Discipline d = _disciplines.get(name);
+    if (d == null) {
+      throw new NoSuchDisciplineNameException(name);
+    }
+
+    return d;
   }
 
   public void removeDiscipline(Discipline d) {
-    _disciplines.remove(d);
+    _disciplines.remove(d.getName());
   }
 
   @Override
@@ -40,7 +55,7 @@ public class Student extends Person {
     String me = _course.isRepresentative(this) ? "DELEGADO" : "ALUNO";
     me += "|" + super.toString();
 
-    for (Discipline d : _disciplines) {
+    for (Discipline d : _disciplines.values()) {
       me += "\n" + d.toString();
     }
 
