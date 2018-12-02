d53c128ed6af1f7b17a8764c11c954dc398af35e
Henrique Dias <hacdias@gmail.com>
2018-12-02 16:24:04 +0000

    fix: test script

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 PROJECTDIR=$1
 
 if [ -z $PROJECTDIR ]; then
-	echo "Usage: ./testAll.sh <project directory>"
+	echo "Usage: ./test.sh <project directory>"
 	exit 0
 fi
 
@@ -13,13 +13,13 @@ if [ ! -d results/ ]; then
 	mkdir results
 fi
 
-for FILE_RAW in $(ls auto-tests/*.in);
+for FILE_RAW in $(ls tests/*.in);
 do
 	FILE=$(echo $FILE_RAW | grep -o -P "A-\d{2}-\d{2}-M-ok")
 	echo Testing $FILE...
-	java -Dimport=auto-tests/$FILE.import -Din=auto-tests/$FILE.in -Dout=results/$FILE.outhyp -cp "/usr/share/java/po-uuilib.jar:$PROJECTDIR/sth-core/sth-core.jar:$PROJECTDIR/sth-app/sth-app.jar" sth.app.App	
+	java -Dimport=tests/$FILE.import -Din=tests/$FILE.in -Dout=results/$FILE.outhyp -cp "/usr/share/java/po-uuilib.jar:$PROJECTDIR/sth-core/sth-core.jar:$PROJECTDIR/sth-app/sth-app.jar" sth.app.App	
 	echo "Testing now the result:"
-	diff -b auto-tests/expected/$FILE.out results/$FILE.outhyp
+	diff -b tests/expected/$FILE.out results/$FILE.outhyp
 done	
 
 echo "Done!"
