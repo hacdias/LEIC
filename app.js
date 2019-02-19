106f1812465f6479eed69316c0771f8d183a667c
Henrique Dias <hacdias@gmail.com>
2019-02-19 10:08:35 +0000

    💄: some improvements on style

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 document.querySelectorAll('.profile').forEach(el => {
   el.addEventListener('click', () => {
-    const src = el.src
-    el.src = el.dataset.url
-    el.dataset.url = src
+    el.classList.toggle('rotate')
   })
 })
