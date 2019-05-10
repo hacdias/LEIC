bdebc35ad81c65fb144b6384a0c6b3934f9d5857
Henrique Dias <hacdias@gmail.com>
2019-05-10 15:01:38 +0100

    feat: novo contacto
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- igo/js/people.js
+++ igo/js/people.js
@@ -73,6 +73,52 @@ function fillCalls (screen, id) {
   console.warn('TODO: Fill previous calls, add call button')
 }
 
+function validateNewPerson () {
+  const name = document.getElementById('new-person-name').value
+  const number = document.getElementById('new-person-phone').value
+
+  if (name.length > 0 && number > 0) {
+    return { name, number }
+  }
+}
+
+function onNewPersonChange () {
+  if (validateNewPerson()) {
+    document.getElementById('new-person-submit').classList.remove('disabled')
+  } else {
+    document.getElementById('new-person-submit').classList.add('disabled')
+  }
+}
+
+function createPerson () {
+  const { name, number } = validateNewPerson()
+
+  confirmationBox({
+    question: 'Confirma a criação de um novo contacto?',
+    rightHandler: () => {
+      data.people.push({
+        id: uuidv4(),
+        name: name,
+        distance: Math.floor(Math.random() * 1000) + 1,
+        picture: 'maria_joao.png', // TODO: photo
+        phone: numberWithSpaces(number),
+        lastSeen: Math.floor(Math.random() * 60) + 1,
+        messages: []
+      })
+
+      runAndBack(clearNewPerson)
+    }
+  })
+}
+
+function clearNewPerson () {
+  document.getElementById('new-person-submit').classList.add('disabled')
+  document.getElementById('new-person-name').value = ''
+  document.getElementById('new-person-phone').value = ''
+}
+
 function bootstrapPeople () {
   enableKeybaordFor(document.getElementById('msg-kb'))
+  enableKeybaordFor(document.getElementById('new-person-name'))
+  enableKeybaordFor(document.getElementById('new-person-phone'), false)
 }
