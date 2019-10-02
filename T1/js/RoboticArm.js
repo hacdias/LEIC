01f4d46fb36b0eaa069c222c2812b52b1f4d844e
Henrique Dias <hacdias@gmail.com>
2019-10-02 16:13:50 +0100

    add fingers

--- T1/js/RoboticArm.js
+++ T1/js/RoboticArm.js
@@ -9,6 +9,24 @@ class Hand extends THREE.Mesh {
     super(geometry, material)
 
     this.rotation.x = Math.PI / 2
+
+    const fingerLeft = this._buildFinger()
+    const fingerRight = this._buildFinger()
+    fingerLeft.position.x = 2
+    fingerLeft.position.y = 4 - height
+    fingerLeft.position.z = 0
+    
+    fingerRight.position.x = -2
+    fingerRight.position.y = 4 - height
+    fingerRight.position.z = 0
+    this.add(fingerLeft)
+    this.add(fingerRight)
+  }
+
+  _buildFinger () {
+    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+    const geometry = new THREE.BoxGeometry(1, 5, 1)
+    return new THREE.Mesh(geometry, material)
   }
 }
 
@@ -17,8 +35,8 @@ class RoboticArm extends THREE.Object3D {
     super()
 
     const jointRadius = depth + 1
-    const handRadius = jointRadius + 1
-    const handHeight = depth
+    const handRadius = jointRadius
+    const handHeight = depth - 1
 
     const armJoint = new Joint(jointRadius)
     armJoint.position.y = forearmLength / 2
