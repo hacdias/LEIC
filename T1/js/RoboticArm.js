75450192a7d3d9bdd0c2a5e7a8b3a91509e4a8ce
Henrique Dias <hacdias@gmail.com>
2019-10-04 11:32:19 +0100

    refactor: hand is a separate file

--- T1/js/RoboticArm.js
+++ T1/js/RoboticArm.js
@@ -1,34 +1,5 @@
-/* global THREE, Arm, Joint */
+/* global THREE, Arm, Joint, Hand */
 'use strict'
-
-class Hand extends THREE.Mesh {
-  constructor ({ radius, height }) {
-    const geometry = new THREE.CylinderGeometry(radius, radius, height, 32)
-    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
-
-    super(geometry, material)
-
-    this.rotation.x = Math.PI / 2
-
-    const fingerLeft = this._buildFinger()
-    const fingerRight = this._buildFinger()
-    fingerLeft.position.x = 2
-    fingerLeft.position.y = 4 - height
-    fingerLeft.position.z = 0
-
-    fingerRight.position.x = -2
-    fingerRight.position.y = 4 - height
-    fingerRight.position.z = 0
-    this.add(fingerLeft)
-    this.add(fingerRight)
-  }
-
-  _buildFinger () {
-    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
-    const geometry = new THREE.BoxGeometry(1, 5, 1)
-    return new THREE.Mesh(geometry, material)
-  }
-}
 
 class RoboticArm extends THREE.Object3D {
   constructor ({ length, depth, forearmLength }) {
