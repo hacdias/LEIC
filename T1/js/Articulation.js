f0506ee3ee7fff72c7b87fa3181d1c0ad19a0be1
Henrique Dias <hacdias@gmail.com>
2019-10-04 11:31:54 +0100

    refactor: articulation is a mesh

--- T1/js/Articulation.js
+++ T1/js/Articulation.js
@@ -1,16 +1,13 @@
 /* global THREE */
 'use strict'
 
-class Articulation extends THREE.Object3D {
+class Articulation extends THREE.Mesh {
   constructor (radius = 7) {
-    super()
-
     const material = new THREE.MeshBasicMaterial({
       color: 0xF0F0F0
     })
 
     const geometry = new THREE.SphereGeometry(radius, 32, 32, 0, 2 * Math.PI, 0, Math.PI / 2)
-    const mesh = new THREE.Mesh(geometry, material)
-    this.add(mesh)
+    super(geometry, material)
   }
 }
