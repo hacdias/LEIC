bb749e5864a6ba594c2575c91d8d7b271cb02c92
Henrique Dias <hacdias@gmail.com>
2019-10-04 11:25:47 +0100

    refactor: arm is a mesh

--- T1/js/Arm.js
+++ T1/js/Arm.js
@@ -1,16 +1,13 @@
 /* global THREE */
 'use strict'
 
-class Arm extends THREE.Object3D {
+class Arm extends THREE.Mesh {
   constructor ({ length, depth }) {
-    super(...arguments)
-
     const geometry = new THREE.BoxGeometry(depth, length, depth)
     const material = new THREE.MeshBasicMaterial({
       color: 0xC0C0C0
     })
 
-    const arm = new THREE.Mesh(geometry, material)
-    this.add(arm)
+    super(geometry, material)
   }
 }
