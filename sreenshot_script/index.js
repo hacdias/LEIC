3e70e5ed92eb17a6f7a2f91fb30fbdffb1a227e6
Henrique Dias <hacdias@gmail.com>
2019-10-18 08:35:06 +0100

    screenshots gen pdf

--- sreenshot_script/index.js
+++ sreenshot_script/index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 'use strict'
 
+const fs = require('fs')
 const puppeteer = require('puppeteer')
+const imagesToPdf = require('images-to-pdf')
 
 const argv = require('yargs')
   .option('ip', { type: 'string', alias: 'i', required: true })
@@ -19,11 +21,20 @@ const urlBuilder = (ip, port, n) => `http://193.136.138.142:58000/index.html?IP=
 async function main () {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+  const screenshots = []
 
   for (let i = 1; i <= 10; i++) {
     await page.goto(urlBuilder(argv.ip, argv.port, i), { waitUntil: 'networkidle2' })
-    await page.screenshot({path: `screenshot-${i}.png`, fullPage: true })
+    const screenshot = `screenshot-${i}.png`
+    await page.screenshot({path: screenshot, fullPage: true })
+    screenshots.push(screenshot)
     await sleep(10000)
+  }
+
+  await imagesToPdf(screenshots, './screenshots.pdf')
+
+  for (const screenshot of screenshots) {
+    fs.unlinkSync(screenshot)
   }
 
   await browser.close()
