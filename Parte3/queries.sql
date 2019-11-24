2f5e9db4803fbf3e1b81fe014b35d556e9623993
Rodrigo Sousa <rod_sousa@hotmail.com>
2019-11-24 16:06:26 +0000

    Update!

--- Parte3/queries.sql
+++ Parte3/queries.sql
@@ -19,32 +19,52 @@ HAVING COUNT (*) >= ALL (
 		FROM incidencia) AS inc
 	GROUP BY latitude, longitude
 )
+ORDER BY nome;
 
---2 --COMO POR 1st SEMESTRE?
+--2
 SELECT email
-FROM (SELECT email AS e
-    FROM utilizador) AS user NATURAL JOIN
-    (SELECT id AS anomalia_id
-	FROM anomalia) AS a NATURAL JOIN
+FROM (SELECT email
+	FROM utilizador_regular) AS u NATURAL JOIN
 	(SELECT anomalia_id
-	FROM incidencia) AS inc
-WHERE YEAR('2019')
+	FROM incidencia) AS inc NATURAL JOIN
+	(SELECT id AS anomalia_id, ts
+	FROM anomalia) AS a NATURAL JOIN
+	(SELECT id AS anomalia_id
+	FROM anomalia_traducao) AS at
+WHERE ts BETWEEN '2019-01-01 00:00:00' AND '2019-06-30 23:59:59'
 GROUP BY email
 HAVING COUNT (*) >= ALL (
-	SELECT COUNT(*)
-	FROM (SELECT email AS e
-        FROM utilizador) AS user NATURAL JOIN
-        (SELECT id AS anomalia_id
-		FROM anomalia) AS a NATURAL JOIN
+	SELECT count(*)
+	FROM (SELECT email
+		FROM utilizador_regular) AS u NATURAL JOIN
 		(SELECT anomalia_id
-		FROM incidencia) AS inc
+		FROM incidencia) AS inc NATURAL JOIN
+		(SELECT id AS anomalia_id, ts
+		FROM anomalia) AS a NATURAL JOIN
+		(SELECT id AS anomalia_id
+		FROM anomalia_traducao) AS at
+	WHERE ts BETWEEN '2019-01-01 00:00:00' AND '2019-06-30 23:59:59'
 	GROUP BY email
+)
+ORDER BY email;
 
-
-
---3
-
-
-
+--3 - Needs better confirmation! And for that a more random popualtion of DB's
+SELECT email
+FROM (SELECT email, COUNT(nome) AS nlocais
+	FROM (SELECT anomalia_id, item_id, email
+		FROM incidencia) AS inc NATURAL JOIN
+		(SELECT id AS anomalia_id
+		FROM anomalia
+		WHERE ts BETWEEN '2019-01-01 00:00:00' AND '2019-12-31 23:59:59') AS a NATURAL JOIN
+		(SELECT id AS item_id, latitude, longitude
+		FROM item
+		WHERE latitude > 39.336775) AS i NATURAL JOIN
+		(SELECT latitude, longitude, nome
+		FROM local_publico) AS lp
+	GROUP BY email) AS c 
+WHERE nlocais = (SELECT COUNT(*)
+	FROM local_publico
+	WHERE latitude > 39.336775)
+;
 
 --4
