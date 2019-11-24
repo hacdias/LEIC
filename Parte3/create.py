4445456717db01967316ab07942dcca472548473
Rodrigo Sousa <rod_sousa@hotmail.com>
2019-11-24 12:57:23 +0000

    Finished Populate!

--- Parte3/create.py
+++ Parte3/create.py
@@ -11,23 +11,23 @@ iterations = 100
 
 print("-- Populate Utilizador")
 for i in range(iterations):
-    print("INSERT INTO utilizador(email, password) VALUES ('Utilizador" + 
+    print("INSERT INTO utilizador(email, password) VALUES ('Email" + 
         str(i) + "', 'password" + str(i) + "');")
 
 print("\n-- Populate Utilizador Qualificado")
 for i in range(0, iterations, 2):
-    print("INSERT INTO utilizador_qualificado(email) VALUES ('Utilizador" + 
+    print("INSERT INTO utilizador_qualificado(email) VALUES ('Email" + 
         str(i) + "');")
 
 print("\n-- Populate Utilizador Regular")
 for i in range(1, iterations, 2):
-    print("INSERT INTO utilizador_regular(email) VALUES ('Utilizador" + 
+    print("INSERT INTO utilizador_regular(email) VALUES ('Email" + 
         str(i) + "');")
 
 iterations = 50
+
 locaisPublicos = []
 counter = 0
-
 print("\n-- Populate Local Publico")
 for latitude in range(-iterations, iterations, 10):
     for longitude in range(-iterations, iterations, 10):
@@ -57,7 +57,7 @@ for i in range(0, iterations, 3):
         str(i) + ", '" + box + "', 'Lingua" + str(i) + "');")
 
 items = []
-print("\n-- Populates duplicado")
+print("\n-- Populates Duplicado")
 for i in range(iterations):
     item2 = randrange(5, 100)
     item1 = randrange(0, item2)
@@ -73,3 +73,25 @@ for i in range(iterations):
     print("INSERT INTO duplicado(item1, item2) VALUES (" +
         str(item1) + ", " + str(item2) + ");")
 
+print("\n-- Populates Incidencia")
+for i in range(0, iterations, 2):
+    itemID = randrange(100)
+    email = randrange(100)
+
+    print("INSERT INTO Incidencia(anomalia_id, item_id, email) VALUES (" +
+        str(i) + ", " + str(itemID) + ", 'Email" + str(email) + "');")
+
+propostasCorrecao = []
+print("\n-- Populate Proposta de Correcao")
+for i in range(0, iterations, 2):
+
+    for x in range(randrange(5)):
+        propostasCorrecao.append(['Email' + str(i), x])
+        print("INSERT INTO proposta_correcao(email, nro, data_hora, texto) VALUES ('Email" +
+            str(i) + "', " + str(x) + ", '" + randomDate(startDate) + "', 'Texto" + str(i) + "');")
+
+print("\n-- Populate Correcao")
+for i in range(0, len(propostasCorrecao), randrange(2) + 1):
+
+    print("INSERT INTO correcao(email, nro, anomalia_id) VALUES ('" +
+            propostasCorrecao[i][0] + "', " + str(propostasCorrecao[i][1]) + ", " + str(randrange(100)) + ");")
