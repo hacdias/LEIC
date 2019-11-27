a297d12278d3f5d868396ce9cd6a0cb74ad6558a
Henrique Dias <hacdias@gmail.com>
2019-11-27 13:25:37 +0000

    remover coisas

--- Parte3/web/anomalies.php
+++ Parte3/web/anomalies.php
@@ -2,9 +2,8 @@
 <html>
 <head>
   <title>Anomalias</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body class="p-4">
+<body>
   <?php $anomalies = getAnomalies();?>
 
   <form method="GET" action="./anomalies_insert.php">
@@ -15,22 +14,18 @@
     <input type="date" name="date" />
     <input type="time" name="time" />
     <input type="text" name="descricao" placeholder="Descrição" />
-    <div class="form-group form-check">
-      <input type="checkbox" class="form-check-input" name="tem_anomalia_redacao" />
-      <label class="form-check-label" for="exampleCheck1">Tem anomalia de redação</label>
-    </div>
-    <input class="btn btn-primary" type="submit" value="Criar" />
+    <input type="checkbox" name="tem_anomalia_redacao" />
+    <label for="exampleCheck1">Tem anomalia de redação</label>
+    <input type="submit" value="Criar" />
   </form>
 
   <form method="GET" action="./anomalies_between_locals.php">
     <h2>Anomalias entre:</h2>
     <?php $locals = getLocals(); ?>
 
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <label class="input-group-text" for="local1">Local 1</label>
-      </div>
-      <select class="custom-select" name="local1" id="local1">
+    <div>
+      <label for="local1">Local 1</label>
+      <select name="local1" id="local1">
         <option selected>Escolha...</option>
         <?php foreach ($locals as $row): ?>
         <option value="<?=$row['latitude']?>,<?=$row['longitude']?>"><?=$row['nome']?> <?=$row['latitude']?>, <?=$row['longitude']?></option>
@@ -38,11 +33,9 @@
       </select>
     </div>
 
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <label class="input-group-text" for="local2">Local 2</label>
-      </div>
-      <select class="custom-select" name="local2" id="local2">
+    <div>
+      <label for="local2">Local 2</label>
+      <select name="local2" id="local2">
         <option selected>Escolha...</option>
         <?php foreach ($locals as $row): ?>
         <option value="<?=$row['latitude']?>,<?=$row['longitude']?>"><?=$row['nome']?> <?=$row['latitude']?>, <?=$row['longitude']?></option>
@@ -50,10 +43,10 @@
       </select>
     </div>
 
-    <input class="btn btn-primary" type="submit" value="Procurar" />
+    <input type="submit" value="Procurar" />
   </form>
 
-  <table class="table">
+  <table>
     <tr>
       <th scope="col">ID</td>
       <th scope="col">Zona</th>
