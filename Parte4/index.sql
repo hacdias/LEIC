d5620c5d9a721691b3645f57c11bd0131e063226
isabelSoares <isabel.srsoares@gmail.com>
2019-12-02 21:00:40 +0000

    maybe 1,2,3 index done

--- Parte4/index.sql
+++ Parte4/index.sql
@@ -1,19 +1,18 @@
 --1
 CREATE INDEX data_hora_index on proposta_de_correcao(data_hora) 
-    using primary KEY
 
 DROP INDEX data_hora_index
 
 
 --2
 CREATE INDEX email_incidencia_index on incidencia(anomalia_id) 
-    using dynamic hashing
+    using hash
 
 DROP INDEX email_incidencia_index
 
 --3
 CREATE INDEX email_correcao_index on correcao(anomalia_id) 
-    using B+-Tree
+    using btree
 
 DROP INDEX email_correcao_index
 
