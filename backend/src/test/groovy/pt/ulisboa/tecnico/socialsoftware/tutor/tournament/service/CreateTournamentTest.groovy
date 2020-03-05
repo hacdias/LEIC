105f66c81e08903b22c48e330d20c8bb614b6195
Claudia Ramires <claudia_raquel888@hotmail.com>
2020-03-05 16:40:57 +0000

    defining some test cases

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/CreateTournamentTest.groovy
@@ -0,0 +1,35 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+import spock.lang.Specification
+
+public class CreateTournamentTest extends Specification {
+
+    def setup() {
+
+    }
+
+    def "create a tournament with one topic and one question"() {
+        expect: false
+    }
+
+    def "create a tournament with ten topics and one question "() {
+        expect: false
+    }
+
+    def "create a tournament with one topic and ten question"() {
+        expect: false
+    }
+
+    def "create a tournament with twelve topic and twelve question"() {
+        expect: false
+    }
+
+    def "create a tournament with an existing name"() {
+        expect: false
+    }
+
+    def "teacher tries to create tournament"() {
+        expect: false
+    }
+
+}
