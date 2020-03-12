20f0b5199737963e7567885ef5397f57875c69e0
Claudia Ramires <claudia_raquel888@hotmail.com>
2020-03-12 15:50:23 +0000

    Define test cases (see #36)

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/StudentTournamentTest.groovy
@@ -0,0 +1,21 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+
+
+
+@DataJpaTest
+public class StudentTournamentTest extends Specification {
+
+    def setup() {
+
+    }
+
+    def "students enrolls in tournament" () {
+        expect: false
+    }
+
+    def "teacher tries to enroll in tournament" () {
+        expect: false
+    }
+
+}
