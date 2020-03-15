fd73ae792db27c9a5b158a67250e6706db0e83a1
Henrique Dias <hacdias@gmail.com>
2020-03-15 09:08:52 +0000

    fix func_decl

--- ast/func_decl_node.h
+++ ast/func_decl_node.h
@@ -10,31 +10,37 @@ namespace og {
    * Class for describing func_decl nodes.
    */
   class func_decl_node: public cdk::basic_node {
-    bool _caracteristic;
-    bool _type;
-    std::string _identifier;
-    cdk::sequence_node *_variables;
-    og::block_node *_block;
+    bool _is_public;
+    bool _is_required;
+    cdk::basic_type *_type;
+    std::string _id;
+    cdk::sequence_node *_args;
 
   public:
-    inline func_decl_node(int lineno, bool &caracteristic, bool &type, std::string &identifier, cdk::sequence_node *variables, og::block_node *block) :
-        cdk::basic_node(lineno), _caracteristic(caracteristic), _type(type), _identifier(identifier), _variables(variables), _block(block) {
+    inline func_decl_node(int lineno, bool is_public, bool is_required,
+      cdk::basic_type *type, std::string *id, cdk::sequence_node *args) :
+        cdk::basic_node(lineno), _is_public(is_public), _is_required(is_required),
+        _type(type), _id(*id), _args(args) {
     }
   public:
-    inline bool &caracteristic() {
-      return _caracteristic;
+    inline bool is_public() {
+      return _is_public;
     }
-    inline bool &type() {
+
+    inline bool is_required() {
+      return _is_required;
+    }
+
+    inline cdk::basic_type *type() {
       return _type;
     }
-    inline std::string &identifier() {
-      return _identifier;
+
+    inline std::string id() {
+      return _id;
     }
-    inline cdk::sequence_node *variables() {
-      return _variables;
-    }
-    inline og::block_node *block() {
-      return _block;
+
+    inline cdk::sequence_node *args() {
+      return _args;
     }
 
     void accept(basic_ast_visitor *sp, int level) {
