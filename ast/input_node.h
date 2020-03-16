34e90cc7e74e8eccecc94c2d8eee921b9f3c75b8
Henrique Dias <hacdias@gmail.com>
2020-03-16 14:53:29 +0000

    input

--- ast/input_node.h
+++ ast/input_node.h
@@ -1,12 +1,14 @@
 #ifndef __OG_AST_INPUT_NODE_H__
 #define __OG_AST_INPUT_NODE_H__
 
+#include <cdk/ast/expression_node.h>
+
 namespace og {
 
   /**
    * Class for describing input nodes.
    */
-  class input_node: public cdk::basic_node {
+  class input_node: public cdk::expression_node {
 
   public:
     inline input_node(int lineno) :
