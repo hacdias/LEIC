123e6b4f4e228408dcfdbc76ec42c74897d6d1a2
isabelSoares <isabel.srsoares@gmail.com>
2020-03-27 18:41:48 +0000

    Starting cam_info and cam_join operations

--- silo-server/src/main/java/pt/tecnico/sauron/silo/Sauron.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/Sauron.java
@@ -1,5 +1,24 @@
 package pt.tecnico.sauron.silo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import pt.tecnico.sauron.silo.grpc.Silo.Coordinates;
+import pt.tecnico.sauron.silo.grpc.Silo.Observation;
+import pt.tecnico.sauron.silo.grpc.Silo.Camera;
+
 public class Sauron {
+
+    private List<Observation> observations = new ArrayList<Observation>();
+    private List<Camera> cameras = new ArrayList<Camera>();
+
+    public Coordinates getCamInfo (String name){
+        return null;
+
+    }
+
+    public void addCamera(Camera camera){
+
+    }
 
 }
--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServiceImpl.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServiceImpl.java
@@ -1,8 +1,31 @@
 package pt.tecnico.sauron.silo;
 
-import pt.tecnico.sauron.silo.grpc.SauronGrpc;
+import io.grpc.stub.StreamObserver;
+import pt.tecnico.sauron.silo.grpc.*;
 
 public class SiloServiceImpl extends SauronGrpc.SauronImplBase{
 
     private Sauron sauron = new Sauron();
+
+    @Override
+    public void CamJoin(Silo.CamJoinRequest request, StreamObserver<Silo.CamJoinResponse> responseObserver){
+
+        sauron.addCamera(request.getCamera());
+        Silo.CamJoinResponse response = Silo.CamJoinResponse.newBuilder().build();
+        
+		responseObserver.onNext(response);
+
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void CamInfo(Silo.CamInfoRequest request,StreamObserver<Silo.CamInfoResponse> responseObserver){
+
+        Silo.CamInfoResponse response = Silo.CamInfoResponse.newBuilder().setCoordinates(sauron.getCamInfo(request.getName())).build();
+        
+		responseObserver.onNext(response);
+
+		responseObserver.onCompleted();
+
+    }
 }
