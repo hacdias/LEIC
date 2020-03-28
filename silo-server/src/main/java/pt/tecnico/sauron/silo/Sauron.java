9ced1224576657e79b3a31127469787fe1e77291
isabelSoares <isabel.srsoares@gmail.com>
2020-03-28 17:28:55 +0000

    T1 - Finish cam_join and cam_info operations

--- silo-server/src/main/java/pt/tecnico/sauron/silo/Sauron.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/Sauron.java
@@ -3,6 +3,7 @@ package pt.tecnico.sauron.silo;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import pt.tecnico.sauron.silo.grpc.Silo.Coordinates;
 import pt.tecnico.sauron.silo.grpc.Silo.Identifier;
 import pt.tecnico.sauron.silo.grpc.Silo.Observation;
@@ -13,14 +14,29 @@ public class Sauron {
 
     private List<Observation> observations = new ArrayList<Observation>();
     private List<Camera> cameras = new ArrayList<Camera>();
+    
+    public void addCamera(Camera camera){
+        Boolean canAdd = true;
+        for (Camera cam: cameras){
+            if (cam.getName() == camera.getName()){
+                canAdd = false;
+            }
+        } 
 
-    public Coordinates getCamInfo (String name){
-        return null;
+        if (canAdd){
+            cameras.add(camera); 
+        }
 
     }
 
-    public void addCamera(Camera camera){
+    public Coordinates getCamInfo(String name){
+        for (Camera cam: cameras){
+            if (cam.getName() == name){
+                return cam.getCoordinates();
+            }
+        }
 
+        return null;
     }
 
     public Observation track(ObservationObject type, Identifier identifier) {
