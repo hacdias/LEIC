f0f63b61a70ad9f1c57dd301e0910877d516c947
isabelSoares <isabel.srsoares@gmail.com>
2020-03-29 17:40:19 +0100

    [T1] Starting client eye

--- eye/src/main/java/pt/tecnico/sauron/eye/EyeApp.java
+++ eye/src/main/java/pt/tecnico/sauron/eye/EyeApp.java
@@ -1,5 +1,10 @@
 package pt.tecnico.sauron.eye;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class EyeApp {
 
@@ -7,10 +12,59 @@ public class EyeApp {
 		System.out.println(EyeApp.class.getSimpleName());
 		
 		// receive and print arguments
+
+		if (args.length != 6) {
+			System.out.printf("Invalid arguments count: %d", args.length);
+			System.exit(1);
+		}
+
 		System.out.printf("Received %d arguments%n", args.length);
 		for (int i = 0; i < args.length; i++) {
 			System.out.printf("arg[%d] = %s%n", i, args[i]);
 		}
+
+		System.out.printf("Server address: %s\n", args[0]);
+		System.out.printf("Server port: %s\n", args[1]);
+		System.out.printf("Camera name: %s\n", args[2]);
+		System.out.printf("Coordinates latitude: %f\n", args[3]);
+		System.out.printf("Coordinates longitude: %f\n", args[4]);
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.printf("> ");
+		String input = scanner.nextLine();
+
+		while (!input.equals("exit")) {
+			List <String> tokens = new ArrayList<String>(Arrays.asList(input.split(",")));
+			if (input.startsWith("#")){
+				System.out.print("> ");
+				input = scanner.nextLine();
+				continue;
+			}
+
+			String firstWord = tokens.remove(0);
+			if (firstWord.equalsIgnoreCase("person")){
+				String identifierPerson = tokens.remove(0);
+			}	
+			
+			if (firstWord.equalsIgnoreCase("car")){
+				String identifierCar = tokens.remove(0);
+			}
+
+			if (firstWord.equalsIgnoreCase("zzz")){
+				Long identifierZzz = Long.parseLong(tokens.remove(0));
+				try{
+					java.util.concurrent.TimeUnit.MILLISECONDS.sleep(identifierZzz);
+				}catch( InterruptedException e ){
+					
+				}
+			}
+
+			System.out.print("> ");
+			input = scanner.nextLine();
+		}
+
+		scanner.close();
 	}
+
 	
 }
