2e8bae310443c6b512fada89ae02dbed582703a3
Henrique Dias <hacdias@gmail.com>
2020-03-29 09:39:18 +0100

    comment entire sauron

--- silo-server/src/main/java/pt/tecnico/sauron/silo/Sauron.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/Sauron.java
@@ -14,7 +14,7 @@ public class Sauron {
     // private List<Observation> observations = new ArrayList<Observation>();
     // private List<Camera> cameras = new ArrayList<Camera>();
 
-    public void addCamera(Camera camera){
+    /*public void addCamera(Camera camera){
         /* Boolean canAdd = true;
         for (Camera cam: cameras){
             if (cam.getName() == camera.getName()){
@@ -24,36 +24,36 @@ public class Sauron {
 
         if (canAdd){
             cameras.add(camera);
-        } */
-    }
+        } 
+    }*/
 
-    public Coordinates getCamInfo(String name){
-        /* for (Camera cam: cameras){
+    /* public Coordinates getCamInfo(String name){
+        for (Camera cam: cameras){
             if (cam.getName() == name){
                 return cam.getCoordinates();
             }
         }
 
-        return null; */
-    }
+        return null; 
+    }*/
 
-    public Observation track(ObservationObject type, Identifier identifier) {
+    /* public Observation track(ObservationObject type, Identifier identifier) {
         return null;
-    }
+    } */
 
-    public Observation trackMatch(ObservationObject type, String partIdentifier) {
+    /* public Observation trackMatch(ObservationObject type, String partIdentifier) {
         return null;
-    }
+    } */
 
-    public List<Observation> trace(ObservationObject type, Identifier identifier) {
+    /* public List<Observation> trace(ObservationObject type, Identifier identifier) {
         return null;
-    }
+    } */
 
-    public void report(String name, List<Observation> observations, List<string> data) {
+    /* public void report(String name, List<Observation> observations, List<string> data) {
         /* Recebe o nome da câmera, um conjunto de observações, e os dados correspondentes.
         O nome deve corresponder a uma câmera registada previamente.
         O servidor regista as observações com a sua data e hora, no momento da receção;
         */
-    }
+    } */
 
 }
