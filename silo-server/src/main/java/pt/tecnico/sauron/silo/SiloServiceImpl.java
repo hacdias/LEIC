0f530917a8640e8b4f6bfb30f9c2f3b9f1f0e0c9
Henrique Dias <hacdias@gmail.com>
2020-03-29 11:28:13 +0100

    implement report

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServiceImpl.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServiceImpl.java
@@ -1,70 +1,107 @@
 package pt.tecnico.sauron.silo;
 
-import pt.tecnico.sauron.silo.domain.Sauron;
+import java.util.Map;
 import io.grpc.stub.StreamObserver;
+
+import pt.tecnico.sauron.silo.domain.ObservationType;
+import pt.tecnico.sauron.silo.domain.Sauron;
+import pt.tecnico.sauron.silo.exceptions.InvalidCameraException;
+import pt.tecnico.sauron.silo.exceptions.InvalidIdentifierException;
 import pt.tecnico.sauron.silo.grpc.*;
 
-public class SiloServiceImpl extends SauronGrpc.SauronImplBase{
-
+public class SiloServiceImpl extends SauronGrpc.SauronImplBase {
     private Sauron sauron = new Sauron();
 
+    private final Map<Silo.ObservationType, ObservationType> typesConverter = Map.ofEntries(
+            Map.entry(Silo.ObservationType.PERSON, ObservationType.PERSON),
+            Map.entry(Silo.ObservationType.CAR, ObservationType.CAR));
+
     @Override
-    public void camJoin(Silo.CamJoinRequest request, StreamObserver<Silo.CamJoinResponse> responseObserver){
+    public void camJoin(Silo.CamJoinRequest request, StreamObserver<Silo.CamJoinResponse> responseObserver) {
 
-        /* sauron.addCamera(request.getCamera());
-        Silo.CamJoinResponse response = Silo.CamJoinResponse.newBuilder().build();
-
-		responseObserver.onNext(response);
-
-        responseObserver.onCompleted(); */
+        /*
+         * sauron.addCamera(request.getCamera()); Silo.CamJoinResponse response =
+         * Silo.CamJoinResponse.newBuilder().build();
+         *
+         * responseObserver.onNext(response);
+         *
+         * responseObserver.onCompleted();
+         */
     }
 
     @Override
-    public void camInfo(Silo.CamInfoRequest request, StreamObserver<Silo.CamInfoResponse> responseObserver){
+    public void camInfo(Silo.CamInfoRequest request, StreamObserver<Silo.CamInfoResponse> responseObserver) {
 
-        /* Silo.CamInfoResponse response = Silo.CamInfoResponse.newBuilder().setCoordinates(sauron.getCamInfo(request.getName())).build();
-
-		responseObserver.onNext(response);
-
-		responseObserver.onCompleted(); */
+        /*
+         * Silo.CamInfoResponse response =
+         * Silo.CamInfoResponse.newBuilder().setCoordinates(sauron.getCamInfo(request.
+         * getName())).build();
+         *
+         * responseObserver.onNext(response);
+         *
+         * responseObserver.onCompleted();
+         */
 
     }
 
     @Override
     public void track(Silo.TrackRequest request, StreamObserver<Silo.TrackResponse> responseObserver) {
 
-        /* Silo.TrackResponse response = Silo.TrackResponse.newBuilder().setObservation(sauron.track(request.getType(), request.getIdentifier())).build();
-
-        responseObserver.onNext(response);
-
-		responseObserver.onCompleted(); */
+        /*
+         * Silo.TrackResponse response =
+         * Silo.TrackResponse.newBuilder().setObservation(sauron.track(request.getType()
+         * , request.getIdentifier())).build();
+         *
+         * responseObserver.onNext(response);
+         *
+         * responseObserver.onCompleted();
+         */
     }
 
     @Override
     public void trackMatch(Silo.TrackMatchRequest request, StreamObserver<Silo.TrackMatchResponse> responseObserver) {
 
-        /* Silo.TrackMatchResponse response = Silo.TrackMatchResponse.newBuilder().setObservation(sauron.trackMatch(request.getType(), request.getPartIdentifier())).build();
-
-        responseObserver.onNext(response);
-
-		responseObserver.onCompleted(); */
+        /*
+         * Silo.TrackMatchResponse response =
+         * Silo.TrackMatchResponse.newBuilder().setObservation(sauron.trackMatch(request
+         * .getType(), request.getPartIdentifier())).build();
+         *
+         * responseObserver.onNext(response);
+         *
+         * responseObserver.onCompleted();
+         */
     }
 
     @Override
     public void trace(Silo.TraceRequest request, StreamObserver<Silo.TraceResponse> responseObserver) {
 
-        /* Silo.TraceResponse response = Silo.TraceResponse.newBuilder().addAllObservations(sauron.trace(request.getType(), request.getIdentifier())).build();
-
-        responseObserver.onNext(response);
-
-		responseObserver.onCompleted(); */
+        /*
+         * Silo.TraceResponse response =
+         * Silo.TraceResponse.newBuilder().addAllObservations(sauron.trace(request.
+         * getType(), request.getIdentifier())).build();
+         *
+         * responseObserver.onNext(response);
+         *
+         * responseObserver.onCompleted();
+         */
     }
 
     @Override
     public void report(Silo.ReportRequest request, StreamObserver<Silo.ReportResponse> responseObserver) {
+        Silo.ReportResponse.Builder builder = Silo.ReportResponse.newBuilder();
 
-        // sauron.report(request.getName(), request.getObservations(), request.getData());
-        // TODO: report nao retorna nada ?
+        try {
+            for (Silo.Observation observation : request.getObservationsList()) {
+                ObservationType type = typesConverter.get(observation.getType());
+                sauron.report(request.getCameraName(), type, observation.getIdentifier());
+            }
+        } catch (InvalidCameraException e) {
+            builder.setStatus(Silo.ResponseStatus.INVALID_CAMERA);
+        } catch (InvalidIdentifierException e) {
+            builder.setStatus(Silo.ResponseStatus.INVALID_IDENTIFIER);
+        }
 
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
     }
 }
