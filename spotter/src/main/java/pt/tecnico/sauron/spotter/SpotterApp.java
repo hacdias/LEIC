8a278831621d16fdaf2310efd7f691a4bcf4d15c
Henrique Dias <hacdias@gmail.com>
2020-03-30 21:42:57 +0100

    feat: implement spotter

--- spotter/src/main/java/pt/tecnico/sauron/spotter/SpotterApp.java
+++ spotter/src/main/java/pt/tecnico/sauron/spotter/SpotterApp.java
@@ -1,12 +1,21 @@
 package pt.tecnico.sauron.spotter;
 
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
-public class SpotterApp {
+import pt.tecnico.sauron.silo.client.domain.Observation;
+import pt.tecnico.sauron.silo.client.domain.ObservationType;
+import pt.tecnico.sauron.silo.client.domain.SiloFrontend;
+import pt.tecnico.sauron.silo.client.exceptions.SauronClientException;
 
+public class SpotterApp {
+	private static Map<String, ObservationType> typesConverter = Map.ofEntries(
+		Map.entry("person", ObservationType.PERSON),
+		Map.entry("car", ObservationType.CAR));
 
 	public static void main(String[] args) {
 		System.out.println(SpotterApp.class.getSimpleName());
@@ -16,9 +25,12 @@ public class SpotterApp {
 			System.exit(1);
 		}
 
-		// TODO: connect with silo-client
-		System.out.printf("Server address: %s\n", args[0]);
-		System.out.printf("Server port: %s\n", args[1]);
+		final String host = args[0];
+		final int port = Integer.parseInt(args[1]);
+		final SiloFrontend api = new SiloFrontend(host, port);
+
+		System.out.printf("Server address: %s\n", host);
+		System.out.printf("Server port: %s\n", port);
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.printf("> ");
@@ -33,25 +45,29 @@ public class SpotterApp {
 
 			String command = tokens.remove(0);
 
-			switch (command) {
-				case "help":
-					help(tokens);
-					break;
-				case "ping":
-					ping(tokens);
-					break;
-				case "clear":
-					clear(tokens);
-					break;
-				case "trail":
-					trail(tokens);
-					break;
-				case "spot":
-					spot(tokens);
-					break;
-				default:
-					System.out.printf("Invalid command: %s\n", input);
-					break;
+			try {
+				switch (command) {
+					case "help":
+						help(tokens, api);
+						break;
+					case "ping":
+						ping(tokens, api);
+						break;
+					case "clear":
+						clear(tokens, api);
+						break;
+					case "trail":
+						trail(tokens, api);
+						break;
+					case "spot":
+						spot(tokens, api);
+						break;
+					default:
+						System.out.printf("Invalid command: %s\n", input);
+						break;
+				}
+			} catch (SauronClientException e) {
+				System.out.println(e);
 			}
 
 			System.out.print("> ");
@@ -61,7 +77,7 @@ public class SpotterApp {
 		scanner.close();
 	}
 
-	private static void help(List<String> tokens) {
+	private static void help(List<String> tokens, SiloFrontend api) {
 		System.out.println("Commands:");
 		System.out.println("\thelp - this command");
 		System.out.println("\texit - stop spotter");
@@ -69,34 +85,35 @@ public class SpotterApp {
 		System.out.println("\ttrail type identifier - fetch observations history");
 		System.out.println("\nControl commands:");
 		System.out.println("\tping - ping the server for status");
-		System.out.print("\tclear - clear the server");
+		System.out.println("\tclear - clear the server");
 	}
 
-	private static void ping(List<String> tokens) {
+	private static void ping(List<String> tokens, SiloFrontend api) throws SauronClientException {
 		if (tokens.size() != 0) {
 			System.out.printf("Invalid arguments: %s. Run 'help' for more information.\n", String.join(" ", tokens));
 			return;
 		}
 
+		System.out.println(api.ping());
 	}
 
-	private static void clear(List<String> tokens) {
+	private static void clear(List<String> tokens, SiloFrontend api) throws SauronClientException {
 		if (tokens.size() != 0) {
 			System.out.printf("Invalid arguments: %s. Run 'help' for more information.\n", String.join(" ", tokens));
 			return;
 		}
 
+		api.clear();
+		System.out.println("Server cleared.");
 	}
 
 	private static Boolean validateTrailSpot(List<String> tokens) {
-		List<String> supportedTypes = List.of("person", "car");
-
 		if (tokens.size() != 2) {
 			System.out.printf("Invalid arguments: %s. Run 'help' for more information.\n", String.join(" ", tokens));
 			return false;
 		}
 
-		if (!supportedTypes.contains(tokens.get(0))) {
+		if (typesConverter.get(tokens.get(0).toLowerCase()) == null) {
 			System.out.printf("Object type of %s is unknown.\n", tokens.get(0));
 			return false;
 		}
@@ -104,12 +121,12 @@ public class SpotterApp {
 		return true;
 	}
 
-	private static void trail(List<String> tokens) {
+	private static void trail(List<String> tokens, SiloFrontend api) throws SauronClientException {
 		if (!validateTrailSpot(tokens)) {
 			return;
 		}
 
-		String type = tokens.get(0);
+		ObservationType type = typesConverter.get(tokens.get(0).toLowerCase());
 		String identifier = tokens.get(1);
 
 		if (identifier.contains("*")) {
@@ -117,31 +134,42 @@ public class SpotterApp {
 			return;
 		}
 
-		// TODO
+
+		List<Observation> observations = api.trace(type, identifier);
+		printFormatted(observations);
 	}
 
-	private static void spot(List<String> tokens) {
+	private static void spot(List<String> tokens, SiloFrontend api) throws SauronClientException {
 		if (!validateTrailSpot(tokens)) {
 			return;
 		}
 
-		String type = tokens.get(0);
+		ObservationType type = typesConverter.get(tokens.get(0).toLowerCase());
 		String identifier = tokens.get(1);
+		List<Observation> observations;
 
 		if (identifier.contains("*")) {
-			// MATCH
+			observations = api.trackMatch(type, identifier);
+		} else {
+			observations = List.of(api.track(type, identifier));
 		}
 
-		// TODO
+		printFormatted(observations);
 	}
 
-	private static void printFormatted(List<Object> observations) {
-		// TODO
-		// Sorted ASC identificador
+	private static void printFormatted(List<Observation> observations) {
+		observations.sort(new Observation.IdentifierComparator());
 
-		for (Object observation: observations) {
-			// Tipo,Identificador,Data-Hora,Nome-Câmera,Latitude-Câmera,Longitude-Câmera
-			// Datetime 8601 c/ segundos
+		for (Observation observation: observations) {
+			System.out.printf(
+				"%s,%s,%s,%s,%f,%f\n",
+				observation.getType(),
+				observation.getIdentifier(),
+				observation.getDatetime().format(DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss")),
+				observation.getCamera().getName(),
+				observation.getCamera().getCoordinates().getLatitude(),
+				observation.getCamera().getCoordinates().getLongitude()
+			);
 		}
 	}
 }
