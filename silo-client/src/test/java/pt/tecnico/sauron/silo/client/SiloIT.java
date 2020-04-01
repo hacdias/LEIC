00e93a043e014f15c523271f2e03ea681783ac8a
Henrique Dias <hacdias@gmail.com>
2020-04-01 09:22:30 +0100

    test: boilerplate tests

--- silo-client/src/test/java/pt/tecnico/sauron/silo/client/SiloIT.java
+++ silo-client/src/test/java/pt/tecnico/sauron/silo/client/SiloIT.java
@@ -1,42 +1,77 @@
 package pt.tecnico.sauron.silo.client;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pt.tecnico.sauron.silo.client.domain.SiloFrontend;
+import pt.tecnico.sauron.silo.client.exceptions.SauronClientException;
 
 public class SiloIT extends BaseIT {
-	
-	// static members
-	// TODO	
-	
-	
+	public static SiloFrontend api;
+
 	// one-time initialization and clean-up
 	@BeforeAll
-	public static void oneTimeSetUp(){
-		
+	public static void oneTimeSetUp() {
+		String host = testProps.getProperty("server.host");
+		Integer port = Integer.parseInt(testProps.getProperty("server.port"));
+		api = new SiloFrontend(host, port);
 	}
 
 	@AfterAll
 	public static void oneTimeTearDown() {
-		
-	}
-	
-	// initialization and clean-up for each test
-	
-	@BeforeEach
-	public void setUp() {
-		
-	}
-	
-	@AfterEach
-	public void tearDown() {
-		
-	}
-		
-	// tests 
-	
-	@Test
-	public void test() {
-		
-		
+		// EMPTY
 	}
 
+	// initialization and clean-up for each test
+
+	@BeforeEach
+	public void setUp() {
+
+	}
+
+	@AfterEach
+	public void tearDown() throws SauronClientException {
+		api.clear();
+	}
+
+	@Test
+	public void testCamJoin() {
+		// TODO
+		assertEquals(true, false, "not implemented");
+	}
+
+	@Test
+	public void testCamInfo() {
+		// TODO
+		assertEquals(true, false, "not implemented");
+	}
+
+	@Test
+	public void testReport() {
+		// TODO
+		assertEquals(true, false, "not implemented");
+	}
+
+	@Test
+	public void testTrack() {
+		// TODO
+		assertEquals(true, false, "not implemented");
+	}
+
+	@Test
+	public void testTrackMatch() {
+		// TODO
+		assertEquals(true, false, "not implemented");
+	}
+
+	@Test
+	public void testTrace() {
+		// TODO
+		assertEquals(true, false, "not implemented");
+	}
 }
