a2e572c9703505fa52d000e435e114f34c09bd15
Rodrigo Sousa <rod_sousa@hotmail.com>
2020-04-01 15:56:54 +0100

    [T5] Tests for T2 done

--- silo-client/src/test/java/pt/tecnico/sauron/silo/client/SiloIT.java
+++ silo-client/src/test/java/pt/tecnico/sauron/silo/client/SiloIT.java
@@ -2,6 +2,10 @@ package pt.tecnico.sauron.silo.client;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -9,12 +13,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import pt.tecnico.sauron.silo.client.domain.Camera;
 import pt.tecnico.sauron.silo.client.domain.Coordinates;
+import pt.tecnico.sauron.silo.client.domain.Observation;
+import pt.tecnico.sauron.silo.client.domain.ObservationType;
 import pt.tecnico.sauron.silo.client.domain.SiloFrontend;
 import pt.tecnico.sauron.silo.client.exceptions.DuplicateCameraException;
 import pt.tecnico.sauron.silo.client.exceptions.InvalidCameraCoordinatesException;
 import pt.tecnico.sauron.silo.client.exceptions.InvalidCameraException;
 import pt.tecnico.sauron.silo.client.exceptions.InvalidCameraNameException;
+import pt.tecnico.sauron.silo.client.exceptions.InvalidIdentifierException;
 import pt.tecnico.sauron.silo.client.exceptions.SauronClientException;
 
 public class SiloIT extends BaseIT {
@@ -88,9 +96,88 @@ public class SiloIT extends BaseIT {
 	}
 
 	@Test
-	public void testReport() {
-		// TODO
-		assertEquals(true, false, "not implemented");
+	public void testReport() throws SauronClientException {
+		String camName = "Camera1";
+		Double latitude = 45.0;
+		Double longitude = 45.0;
+		Coordinates coordinates = new Coordinates(latitude, longitude);
+		Camera camera = new Camera(camName, coordinates);
+		api.camJoin(camName, latitude, longitude);
+
+		List<Observation> observations = new ArrayList<Observation>();
+		ObservationType type1 = ObservationType.PERSON;
+		String identifier1 = "12345";
+		Observation observation1 = new Observation(camera, type1, identifier1, LocalDateTime.now());
+		observations.add(observation1);
+		ObservationType type2 = ObservationType.CAR;
+		String identifier2 = "AA22AA";
+		Observation observation2 = new Observation(camera, type2, identifier2, LocalDateTime.now());
+		observations.add(observation2);
+
+		api.report(camName, observations);
+	}
+
+	public void testReportNoRegisteredCamera() throws SauronClientException {
+		String camName = "Camera1";
+		Double latitude = 45.0;
+		Double longitude = 45.0;
+		Coordinates coordinates = new Coordinates(latitude, longitude);
+		Camera camera = new Camera(camName, coordinates);;
+
+		List<Observation> observations = new ArrayList<Observation>();
+		ObservationType type1 = ObservationType.PERSON;
+		String identifier1 = "12345";
+		Observation observation1 = new Observation(camera, type1, identifier1, LocalDateTime.now());
+		observations.add(observation1);
+		ObservationType type2 = ObservationType.CAR;
+		String identifier2 = "AA22AA";
+		Observation observation2 = new Observation(camera, type2, identifier2, LocalDateTime.now());
+		observations.add(observation2);
+
+		Assertions.assertThrows(InvalidCameraException.class, () -> api.report(camName, observations));
+	}
+
+	@Test
+	public void testReportInvalidPersonIdentifier() throws SauronClientException {
+		String camName = "Camera1";
+		Double latitude = 45.0;
+		Double longitude = 45.0;
+		Coordinates coordinates = new Coordinates(latitude, longitude);
+		Camera camera = new Camera(camName, coordinates);
+		api.camJoin(camName, latitude, longitude);
+
+		List<String> identifiers = List.of("AAAA", "1A1");
+
+		ObservationType type = ObservationType.PERSON;
+		
+		for (String identifier : identifiers) {
+			List<Observation> observations = new ArrayList<Observation>();
+			Observation observation = new Observation(camera, type, identifier, LocalDateTime.now());
+			observations.add(observation);
+			Assertions.assertThrows(InvalidIdentifierException.class, () -> api.report(camName, observations), "Failed on: " + identifier);
+		}
+	}
+
+	@Test
+	public void testReportInvalidCarIdentifier() throws SauronClientException {
+		String camName = "Camera1";
+		Double latitude = 45.0;
+		Double longitude = 45.0;
+		Coordinates coordinates = new Coordinates(latitude, longitude);
+		Camera camera = new Camera(camName, coordinates);
+		api.camJoin(camName, latitude, longitude);
+
+		List<String> identifiers = List.of("AAZZAA", "112211", "11A211", "1ABBCC");
+
+		ObservationType type = ObservationType.CAR;
+		
+		for (String identifier : identifiers) {
+			List<Observation> observations = new ArrayList<Observation>();
+			Observation observation = new Observation(camera, type, identifier, LocalDateTime.now());
+			observations.add(observation);
+			Assertions.assertThrows(InvalidIdentifierException.class, () -> api.report(camName, observations), "Failed on: " + identifier);
+		}
+
 	}
 
 	@Test
