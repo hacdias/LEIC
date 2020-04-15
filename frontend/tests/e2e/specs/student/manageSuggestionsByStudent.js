60b785c57233a99e37c59959264c87c3297d4f1a
Henrique Dias <hacdias@gmail.com>
2020-04-15 08:27:41 +0100

    test: create two suggestions and delete them

--- frontend/tests/e2e/specs/student/manageSuggestionsByStudent.js
+++ frontend/tests/e2e/specs/student/manageSuggestionsByStudent.js
@@ -30,4 +30,47 @@ describe('Suggestions walkthrough', () => {
 
     cy.deleteSuggestion('Test-Question');
   });
+
+  it('login creates two suggestions and deletes them', () => {
+    cy.createSuggestion('Test-Question1', 'Test Question Content', [
+      {
+        correct: false,
+        content: 'Option 1'
+      },
+      {
+        correct: false,
+        content: 'Option 2'
+      },
+      {
+        correct: true,
+        content: 'Option 3'
+      },
+      {
+        correct: false,
+        content: 'Option 4'
+      }
+    ]);
+
+    cy.createSuggestion('Test-Question2', 'Test Question Content', [
+      {
+        correct: false,
+        content: 'Option 1'
+      },
+      {
+        correct: false,
+        content: 'Option 2'
+      },
+      {
+        correct: true,
+        content: 'Option 3'
+      },
+      {
+        correct: false,
+        content: 'Option 4'
+      }
+    ]);
+
+    cy.deleteSuggestion('Test-Question1');
+    cy.deleteSuggestion('Test-Question2');
+  })
 });
