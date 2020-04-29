973aad52189a94754d42b5137d134f3283b061b5
Henrique Dias <hacdias@gmail.com>
2020-04-29 20:52:24 +0100

    styles: cleanup indentation

--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/LogElement.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/LogElement.java
@@ -5,25 +5,29 @@ import java.util.List;
 import pt.tecnico.sauron.silo.grpc.Silo;
 
 public class LogElement {
-    private List<Integer> replicaTimestamp;
-    private Silo.CamJoinRequest camRequest;
-    private Silo.ReportRequest reportRequest;
-
-    public LogElement(List<Integer> timestamp, Silo.CamJoinRequest request) {
-        this.replicaTimestamp = timestamp;
-        this.camRequest = request;
-    }
-
-    public LogElement(List<Integer> timestamp, Silo.ReportRequest request) {
-        this.replicaTimestamp = timestamp;
-        this.reportRequest = request;
-    }
-
-    public Silo.CamJoinRequest getCamJoinRequest() {
-        return camRequest;
-    }
-
-    public Silo.ReportRequest getReportRequest() {
-        return reportRequest;
-    }
+  private List<Integer> replicaTimestamp;
+  private Silo.CamJoinRequest camRequest;
+  private Silo.ReportRequest reportRequest;
+  
+  public LogElement(List<Integer> timestamp, Silo.CamJoinRequest request) {
+    this.replicaTimestamp = timestamp;
+    this.camRequest = request;
+  }
+  
+  public LogElement(List<Integer> timestamp, Silo.ReportRequest request) {
+    this.replicaTimestamp = timestamp;
+    this.reportRequest = request;
+  }
+  
+  public List<Integer> getReplicaTimestamp() {
+    return replicaTimestamp;
+  }
+  
+  public Silo.CamJoinRequest getCamJoinRequest() {
+    return camRequest;
+  }
+  
+  public Silo.ReportRequest getReportRequest() {
+    return reportRequest;
+  }
 }
--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/ReplicaManager.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/ReplicaManager.java
@@ -8,53 +8,54 @@ import java.util.concurrent.TimeUnit;
 import pt.tecnico.sauron.silo.grpc.Silo;
 
 public class ReplicaManager {
-    private Integer instance;
-    private List<Integer> replicaTimestamp = Collections.synchronizedList(new ArrayList<Integer>());
-    private List<Integer> valueTimestamp = Collections.synchronizedList(new ArrayList<Integer>());
-    private List<LogElement> log = Collections.synchronizedList(new ArrayList<LogElement>());
-    private List<List<Integer>> operationTable = Collections.synchronizedList(new ArrayList<List<Integer>>());
+  private Integer instance;
+  private List<Integer> replicaTimestamp = Collections.synchronizedList(new ArrayList<Integer>());
+  private List<Integer> valueTimestamp = Collections.synchronizedList(new ArrayList<Integer>());
+  private List<LogElement> log = Collections.synchronizedList(new ArrayList<LogElement>());
+  private List<List<Integer>> operationTable = Collections.synchronizedList(new ArrayList<List<Integer>>());
 
-    public ReplicaManager(Integer instance, Integer numberServers) {
-        this.instance = instance;
+  public ReplicaManager(Integer instance, Integer numberServers) {
+    this.instance = instance;
 
-        for (Integer i = 0; i < numberServers; i++) {
-            this.replicaTimestamp.add(0);
-            this.valueTimestamp.add(0);
-        }
+    for (Integer i = 0; i < numberServers; i++) {
+      this.replicaTimestamp.add(0);
+      this.valueTimestamp.add(0);
+    }
+  }
+
+  private Boolean timestampGreaterOrEquals(List<Integer> prev) {
+    for (Integer i = 0; i < prev.size(); i++) {
+      if (prev.get(i) > this.valueTimestamp.get(i)) {
+        return false;
+      }
     }
 
-    private Boolean timestampGreaterOrEquals(List<Integer> prev) {
-        for (Integer i = 0; i < prev.size(); i++) {
-            if (prev.get(i) > this.valueTimestamp.get(i))
-                return false;
-        }
+    return true;
+  }
 
-        return true;
+  public List<Integer> read(List<Integer> prev) {
+    while (!this.timestampGreaterOrEquals(prev)) {
+      try {
+        TimeUnit.MILLISECONDS.sleep(100);
+      } catch (InterruptedException e) {}
+    }
+    return new ArrayList<Integer>(this.valueTimestamp);
+  }
+
+  public List<Integer> modify(List<Integer> prev, Object request) {
+    if (!this.operationTable.contains(prev)) {
+      this.replicaTimestamp.set(this.instance - 1, this.replicaTimestamp.get(this.instance - 1) + 1);
+
+      prev.set(this.instance - 1, this.replicaTimestamp.get(this.instance - 1));
+
+      if (request instanceof Silo.CamJoinRequest) {
+        log.add(new LogElement(prev, (Silo.CamJoinRequest)request));
+      } else if (request instanceof Silo.ReportRequest) {
+        log.add(new LogElement(prev, (Silo.ReportRequest)request));
+      }
+      return prev;
     }
 
-    public List<Integer> read(List<Integer> prev) {
-        while (!this.timestampGreaterOrEquals(prev)) {
-            try {
-                TimeUnit.MILLISECONDS.sleep(100);
-            } catch( InterruptedException e ){
-            }
-        }
-        return new ArrayList<Integer>(this.valueTimestamp);
-    }
-
-    public List<Integer> modify(List<Integer> prev, Object request) {
-        if (!this.operationTable.contains(prev)) {
-            this.replicaTimestamp.set(this.instance - 1, this.replicaTimestamp.get(this.instance - 1) + 1);
-
-            prev.set(this.instance - 1, this.replicaTimestamp.get(this.instance - 1));
-
-            if (request instanceof Silo.CamJoinRequest)
-                log.add(new LogElement(prev, (Silo.CamJoinRequest)request));
-            else if (request instanceof Silo.ReportRequest)
-                log.add(new LogElement(prev, (Silo.ReportRequest)request));
-            return prev;
-        }
-
-        return prev;
-    }
+    return prev;
+  }
 }
