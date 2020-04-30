284e565193000119866408bc48f51d8aab53ce73
Henrique Dias <hacdias@gmail.com>
2020-04-30 13:44:21 +0100

    refactor: cleanup eye

--- eye/src/main/java/pt/tecnico/sauron/eye/EyeApp.java
+++ eye/src/main/java/pt/tecnico/sauron/eye/EyeApp.java
@@ -1,118 +1,101 @@
 package pt.tecnico.sauron.eye;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-
-import pt.tecnico.sauron.silo.client.domain.Observation;
-import pt.tecnico.sauron.silo.client.domain.Camera;
-import pt.tecnico.sauron.silo.client.domain.Coordinates;
-import pt.tecnico.sauron.silo.client.domain.ObservationType;
-import pt.tecnico.sauron.silo.client.domain.SiloFrontend;
+import pt.tecnico.sauron.silo.client.domain.*;
 import pt.tecnico.sauron.silo.client.exceptions.SauronClientException;
 
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class EyeApp {
+    private static Map<String, ObservationType> typesConverter = Map.ofEntries(
+        Map.entry("person", ObservationType.PERSON),
+        Map.entry("car", ObservationType.CAR)
+    );
 
-	private static Map<String, ObservationType> typesConverter = Map.ofEntries(
-		Map.entry("person", ObservationType.PERSON),
-		Map.entry("car", ObservationType.CAR));
+    public static void main(String[] args) {
+        System.out.println(EyeApp.class.getSimpleName());
 
-	public static void main(String[] args) {
-		System.out.println(EyeApp.class.getSimpleName());
+        final List<Observation> observations = new ArrayList<>();
+        final Camera camera;
 
-		final List<Observation> observations = new ArrayList<Observation>();
-		final Camera camera;
+        if (args.length < 5 || args.length > 6) {
+            System.out.printf("Invalid arguments count: %d\n", args.length);
+            System.exit(1);
+        }
 
-		// receive and print arguments
+        final String zooHost = args[0];
+        final Integer zooPort = Integer.parseInt(args[1]);
 
-		if (args.length < 5 || args.length > 6) {
-			System.out.printf("Invalid arguments count: %d", args.length);
-			System.exit(1);
-		}
+        final String cameraName = args[2];
+        final Double coordinatesLatitude = Double.parseDouble(args[3]);
+        final Double coordinatesLongitude = Double.parseDouble(args[4]);
 
-		final String zooHost = args[0];
-		final Integer zooPort = Integer.parseInt(args[1]);
+        int instance = -1;
+        if (args.length == 6)
+            instance = Integer.parseInt(args[5]);
 
-		final String cameraName = args[2];
-		final Double coordinatesLatitude = Double.parseDouble(args[3]);
-		final Double coordinatesLongitude = Double.parseDouble(args[4]);
+        try (SiloFrontend api = new SiloFrontend(zooHost, zooPort, instance)) {
+            try {
+                System.out.print("Registering camera...");
+                api.camJoin(cameraName, coordinatesLatitude, coordinatesLongitude);
+                System.out.println(" done");
+            } catch (SauronClientException e) {
+                System.err.println(e.toString());
+                api.close();
+                System.exit(1);
+            }
 
-		Integer instance = -1;
-		if (args.length == 6)
-			instance = Integer.parseInt(args[5]);
-	
-		final SiloFrontend api = new SiloFrontend(zooHost, zooPort, instance);
-		try {
-			api.camJoin(cameraName, coordinatesLatitude, coordinatesLongitude);
-		} catch(SauronClientException e){
-			System.out.println(e);
-			api.close();
-			System.exit(1);
-		}
+            Coordinates coordinates = new Coordinates(coordinatesLatitude, coordinatesLongitude);
+            camera = new Camera(cameraName, coordinates);
 
-		Coordinates coordinates = new Coordinates(coordinatesLatitude, coordinatesLongitude);
-		camera = new Camera(cameraName, coordinates);
-		
-		Scanner scanner = new Scanner(System.in);
-		String input = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
 
-		while (input != null && !input.equals("exit")) {
-			List <String> tokens = new ArrayList<String>(Arrays.asList(input.split(",")));
-			if (input.startsWith("#")){
-				input = scanner.nextLine();
-				continue;
-			}
+            while (input != null && !input.equals("exit")) {
+                List<String> tokens = new ArrayList<>(Arrays.asList(input.split(",")));
+                if (input.startsWith("#")) {
+                    input = scanner.nextLine();
+                    continue;
+                }
 
-			String firstWord = tokens.remove(0).toLowerCase();
-			if (firstWord.equals("person") || firstWord.equals("car")){
-				String identifier = tokens.remove(0);
-				observations.add(createObservation(camera, firstWord, identifier));
-			}
+                String firstWord = tokens.remove(0).toLowerCase();
 
-			else if (input.isBlank()){
-				try {
-					api.report(camera.getName(), observations);
-				} catch (SauronClientException e) {
-					System.out.println(e);
-				}
-				observations.clear();
-			}
+                if (firstWord.equals("person") || firstWord.equals("car")) {
+                    String identifier = tokens.remove(0);
+                    observations.add(createObservation(camera, firstWord, identifier));
+                } else if (input.isBlank()) {
+                    try {
+                        api.report(camera.getName(), observations);
+                    } catch (SauronClientException e) {
+                        System.err.println(e.toString());
+                    }
+                    observations.clear();
+                } else if (firstWord.equals("zzz")) {
+                    long identifierZzz = Long.parseLong(tokens.remove(0));
+                    try {
+                        Thread.sleep(identifierZzz);
+                    } catch (InterruptedException ignored) {
+                    }
+                } else {
+                    System.out.println("Invalid input");
+                }
 
-			else if (firstWord.equals("zzz")){
-				Long identifierZzz = Long.parseLong(tokens.remove(0));
-				try{
-					java.util.concurrent.TimeUnit.MILLISECONDS.sleep(identifierZzz);
-				}catch( InterruptedException e ){
+                if (scanner.hasNextLine()) input = scanner.nextLine();
+                else input = null;
+            }
 
-				}
-			}
+            scanner.close();
+            try {
+                api.report(camera.getName(), observations);
+            } catch (SauronClientException e) {
+                System.out.println(e);
+            }
+            observations.clear();
+        }
+    }
 
-			else {
-				System.out.println("Invalid input");
-			}
-			
-			if (scanner.hasNextLine()){
-				input = scanner.nextLine();
-			} else { input = null; }
-		}
-
-		scanner.close();
-		try {
-			api.report(camera.getName(), observations);
-		} catch (SauronClientException e) {
-			System.out.println(e);
-		}
-		observations.clear();
-		api.close();
-	}
-
-	public static Observation createObservation(Camera camera, String type, String identifier) {
-		ObservationType observationType = typesConverter.get(type);
-		Observation observation = new Observation(camera, observationType, identifier, LocalDateTime.now());	
-		return observation;	
-	}
+    public static Observation createObservation(Camera camera, String type, String identifier) {
+        ObservationType observationType = typesConverter.get(type);
+        return new Observation(camera, observationType, identifier, LocalDateTime.now());
+    }
 }
